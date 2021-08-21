94270d970ae4863c554cf6cd133e3e9b496ec2ff
DanA <Danny13653@gmail.com>
2021-08-21 15:23:11 +0100

    Try Catch

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,19 @@ import LabmakerAPI from './APIHandler';
 const id = '3630aeb2-38c5-4c36-a0d5-5c2d95fa35b0';
 
 (async () => {
-  const config = await LabmakerAPI.Reddit.getOne(id);
-  console.log(config);
-  const client = new Snoowrap({
-    userAgent: config.userAgent,
-    clientId: config.clientId,
-    clientSecret: config.clientSecret,
-    username: config.username,
-    password: config.password,
-  });
+  try {
+    const config = await LabmakerAPI.Reddit.getOne(id);
+    console.log(config);
+    const client = new Snoowrap({
+      userAgent: config.userAgent,
+      clientId: config.clientId,
+      clientSecret: config.clientSecret,
+      username: config.username,
+      password: config.password,
+    });
 
-  createEvent(client, id);
+    createEvent(client, id);
+  } catch (err) {
+    console.log(err);
+  }
 })();
