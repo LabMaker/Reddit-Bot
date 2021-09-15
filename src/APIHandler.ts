2e9b5388086aee9806f6acf50d976e7a26263889
DanA <22224922+DNYLA@users.noreply.github.com>
2021-09-15 16:22:29 +0100

    Updated API Package

--- src/APIHandler.ts
+++ src/APIHandler.ts
@@ -1,3 +1,5 @@
 import LabmakerAPI from 'labmaker-api-wrapper';
 
-export const Labmaker = new LabmakerAPI(process.env.API_URL);
+export const Labmaker = new LabmakerAPI(process.env.API_URL, {
+  debug: process.env.API_DEBUG === 'true',
+});
