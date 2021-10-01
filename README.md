cc1e285ec7532f3f3a3bad7cbe8e8c2d85c3ffca
DanA <22224922+DNYLA@users.noreply.github.com>
2021-10-01 15:28:30 +0100

    Added EcoSystem.js File

--- README.md
+++ README.md
@@ -6,3 +6,6 @@
 2. `npm run-script build`
 3. `npm install pm2 -g`
 4. `pm2 start ecosystem.config.js`
+
+##Notes
+Dont Add a / to the end of your API_URL environment variable.
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,11 @@ module.exports = {
       name: 'RedditBot',
       script: 'lib/index.js',
       node_args: '-r dotenv/config',
+      env: {
+        API_URL: 'http://localhost:3000',
+        API_TOKEN: 'TOKEN',
+        API_DEBUG: 'false',
+      },
     },
   ],
 };
