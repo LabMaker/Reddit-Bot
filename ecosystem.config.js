97c7081242daf82467a7a4518c346ae2839da5a4
DanA <22224922+DNYLA@users.noreply.github.com>
2021-12-06 13:46:23 +0000

    Installed PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,9 +5,14 @@ module.exports = {
       script: 'lib/index.js',
       node_args: '-r dotenv/config',
       env: {
-        API_URL: 'http://localhost:3000',
-        API_TOKEN: 'TOKEN',
-        API_DEBUG: 'false',
+        //You Can add ENV Variables here or in .env
+        // API_URL: 'http://localhost:3000',
+        // API_TOKEN: 'TOKEN',
+        // API_DEBUG: 'false',
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
       },
     },
   ],
